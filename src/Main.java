60a61d9654a31e13d41e84ae7a4f8177aa9cf62d
Babkin Gennadiy <babkin@kursksu.ru>
2023-01-30 00:19:13 +0300

    Подготовка графической части

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        JFrame f = new JFrame("Swing Paint Demo");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MyPanel myPanel = new MyPanel();
+        myPanel.repaint();
+        f.add(myPanel);
+        f.pack();
+        f.setVisible(true);
     }
 }
---
+++ src/MyPanel.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyPanel extends JPanel {
+    public MyPanel() {
+        setBorder(BorderFactory.createLineBorder(Color.black));
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(1560,700);
+    }
+    private int x = 0;
+    private int y = 0;
+    private int width = 100;
+    private int height = 100;
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Draw Text
+        //g.drawString("This is my custom Panel!",10,20);
+        g.drawLine(x,y,width, height);
+    }
+
+}
