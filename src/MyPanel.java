ddee7d7258be5102a49e6272f04382303b260d93
Babkin Gennadiy <babkin@kursksu.ru>
2023-01-31 15:07:25 +0300

    Ввод данных во время работы программы

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -72,4 +72,23 @@ public class MyPanel extends JPanel {
         }
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
 }
