2ae08e94d63e7274d4fdd5ae072ea9cccebb967d
Babkin Gennadiy <babkin@kursksu.ru>
2023-01-31 23:55:19 +0300

    Примитив вывод линии

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +8,7 @@ public class Main {
         JFrame f = new JFrame("Swing Paint Demo");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MyPanel myPanel = new MyPanel();
+        myPanel.line(new Point(100, 200), new Point(200, 150));
         myPanel.repaint();
         f.add(myPanel);
         f.pack();
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -13,13 +13,21 @@ public class MyPanel extends JPanel {
     private int y = 0;
     private int width = 100;
     private int height = 100;
+    private  Point from = new Point(0, 0);
+    private  Point to = new Point(0, 0);
+    public void line(Point from, Point to){
+        this.from = from;
+        lineTo(to);
+    }
+    public void lineTo(Point to){
+        this.to = to;
+    }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         // Draw Text
         //g.drawString("This is my custom Panel!",10,20);
-        g.drawLine(x,y,width, height);
+        g.drawLine(from.x, from.y, to.x, to.y);
     }
-
 }
